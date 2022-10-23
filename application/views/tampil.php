9aab481314e0f6f06cc57dc53955127b20a4c461
gtpesantren22 <cakromat26@gmail.com>
2022-10-23 16:50:26 +0700

    Uploads

---
+++ application/views/tampil.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-OERcA2EqjJCMA+/3y+gxIOqMEjwtxJY7qPCqsdltbNJuaOe923+mo//f6V8Qbsw3" crossorigin="anonymous"></script>
+
+</head>
+
+<body>
+    <div class="container-fluid">
+        <nav class="navbar" style="background-color: #e3f2fd;">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <p class="card-text"> <img src="<?= base_url('bootstrap-logo.svg') ?>" alt="Logo" width="30" height="24" class="d-inline-block align-text-top">
+                        Surat Resmi PP. Darul Lughah Wal Karomah
+                    </p>
+                </a>
+            </div>
+        </nav>
+        <div class="row">
+            <div class="col-md-4">
+                <div class="card mt-2">
+                    <div class="card-header">
+                        Identitas Surat
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Identitas Surat</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="card mt-2">
+                    <div class="card-header">
+                        Surat Resmi
+                    </div>
+                    <div class="card-body">
+                        <h5 class="card-title">Surat Resmi</h5>
+                        <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+                        <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
