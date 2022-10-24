ee17c35b3f4f6f82dfebfad7baee415bd52e380f
gtpesantren22 <cakromat26@gmail.com>
2022-10-24 09:05:10 +0700

    Update koneksi

--- application/config/database.php
+++ application/config/database.php
@@ -76,9 +76,15 @@ $query_builder = TRUE;
 $db['default'] = array(
 	'dsn'	=> '',
 	'hostname' => 'localhost',
+
+	// 'username' => 'u9048253_dwk',
+	// 'password' => 'PesantrenDWKIT2021',
+	// 'database' => 'u9048253_sekretaris',
+
 	'username' => 'root',
 	'password' => '',
 	'database' => 'db_sekretaris',
+
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
